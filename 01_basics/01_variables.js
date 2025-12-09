9e9eec8b6e0ac2e27881257cd863cd2ee58f40ff
FUHARPARDHI <165705386+FUHARPARDHI@users.noreply.github.com>
2025-12-09 20:02:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = "144553"
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+
+
+accountEmail = "hc@g.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+console.log(accountId);
+
+console.table([accountId ,accountPassword, accountEmail,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+//alert(3+3)
+
+console.log(3+3); 
+console.log("fuhar")
+let name = " hitesh"
+let age=18
+let loggedIn = false
+let state;
+ console.log(typeof "hitesh")
+ console.log(typeof unefined)
+ console.log(typeof null)
+
+

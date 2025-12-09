90bcad8e53d88f14f8ba7ad36199a90408f062b9
FUHARPARDHI <165705386+FUHARPARDHI@users.noreply.github.com>
2025-12-09 20:20:37 +0000

    conversion

---
+++ 01_basics/03_converion.js
@@ -0,0 +1,12 @@
+let score= "33"
+console.log(typeof score);
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let isLoggedIn =1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+let someNumber =33
+let StringNumber =String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber);
